1dc123f06cb6c00810823a6ecda6d5f240f9b924
kento <na.ken0627@gmail.com>
2025-05-23 23:13:32 +0900

    fix:ColorPicker

--- app/050_MyPractice/components/ColorPicker.tsx
+++ app/050_MyPractice/components/ColorPicker.tsx
@@ -4,13 +4,38 @@ import React, { useState } from "react";
 
 function ColorPicker() {
   //# 選択された色を管理するstateを用意しよう
+  const [displayColor, setDisplayColor] = useState<string>("");
 
   //# select要素が変化したときのハンドラ関数を追加しよう
+  const handleColor = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    switch (e.target.value) {
+      case "red":
+        setDisplayColor("bg-red-500");
+        break;
+      case "green":
+        setDisplayColor("bg-green-500");
+        break;
+      case "blue":
+        setDisplayColor("bg-blue-500");
+        break;
+    }
+  };
 
   return (
     <div>
       {/* select要素を作成し、色を選択できるようにしよう */}
+      <select
+        name=""
+        id="color"
+        className="border border-amber-50"
+        onChange={handleColor}
+      >
+        <option value="red">赤色</option>
+        <option value="blue">青色</option>
+        <option value="green">緑色</option>
+      </select>
       {/* 色に応じて背景色が切り替わるdivを作ろう */}
+      <div className={`w-10 h-10 ${displayColor || "bg-amber-50"} `}></div>
     </div>
   );
 }
