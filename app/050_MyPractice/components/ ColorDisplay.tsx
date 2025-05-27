96393dbf105fa9aca3e6e24824560ea3c40517fc
kento <na.ken0627@gmail.com>
2025-05-27 23:35:54 +0900

    add input

--- app/050_MyPractice/components/ ColorDisplay.tsx
+++ app/050_MyPractice/components/ ColorDisplay.tsx
@@ -13,6 +13,11 @@ function ColorDisplay() {
         placeholder="例:#ffffff"
         onChange={(e) => setInputColor(e.target.value)}
       />
+      <input
+        type="text"
+        placeholder="例:#ffffff"
+        onChange={(e) => setInputColor(e.target.value)}
+      />
       <div
         className="my-3 w-10 h-10"
         style={{ backgroundColor: inputColor }}
