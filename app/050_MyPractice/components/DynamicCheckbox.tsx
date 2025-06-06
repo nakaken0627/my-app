76fa0da63a73d33ce86071e18e7240645dd528fe
kento <na.ken0627@gmail.com>
2025-06-06 22:48:53 +0900

    fix:add some logic

--- app/050_MyPractice/components/DynamicCheckbox.tsx
+++ app/050_MyPractice/components/DynamicCheckbox.tsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DynamicCheckbox = () => {
   // 選択されたアイテムを管理するstateをここに作成します
-
+  // const [checkedItem, setCheckedItem] = useState([]);
   // チェックボックスの状態が変化したときの処理をここに記述します
+
+  const handleChecked = (e) => {
+    console.log(e.target.value);
+  };
 
   return (
     <div>
       <label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          name="item"
+          value="potato"
+          onChange={handleChecked}
+        />
         ポテト🍟
       </label>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" name="item" value="hamburger" />
         ハンバーガー🍔
       </label>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" name="item" value="pizza" />
         ピザ🍕
       </label>
       <div>{/* ここに選択されたアイテムを表示します */}</div>
