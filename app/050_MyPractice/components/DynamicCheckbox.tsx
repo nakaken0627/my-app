5fad7a4732594b7805309da2a8c74e50db43b79e
kento <na.ken0627@gmail.com>
2025-06-06 23:12:51 +0900

    fix:add func for complete

--- app/050_MyPractice/components/DynamicCheckbox.tsx
+++ app/050_MyPractice/components/DynamicCheckbox.tsx
@@ -1,12 +1,21 @@
+"use client";
+
+// import React from "react";
 import React, { useState } from "react";
 
 const DynamicCheckbox = () => {
   // 選択されたアイテムを管理するstateをここに作成します
-  // const [checkedItem, setCheckedItem] = useState([]);
+  const [checkedItem, setCheckedItem] = useState<string[]>([]);
   // チェックボックスの状態が変化したときの処理をここに記述します
 
-  const handleChecked = (e) => {
-    console.log(e.target.value);
+  const handleChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checkedItem = e.target.value;
+
+    if (e.target.checked) {
+      setCheckedItem((prev) => [...prev, checkedItem]);
+    } else {
+      setCheckedItem((prev) => prev.filter((i) => i != checkedItem));
+    }
   };
 
   return (
@@ -15,20 +24,37 @@ const DynamicCheckbox = () => {
         <input
           type="checkbox"
           name="item"
-          value="potato"
+          value="ポテト🍟"
           onChange={handleChecked}
+          checked={checkedItem.includes("ポテト🍟")}
         />
         ポテト🍟
       </label>
       <label>
-        <input type="checkbox" name="item" value="hamburger" />
+        <input
+          type="checkbox"
+          name="item"
+          value="ハンバーガー🍔"
+          onChange={handleChecked}
+          checked={checkedItem.includes("ハンバーガー🍔")}
+        />
         ハンバーガー🍔
       </label>
       <label>
-        <input type="checkbox" name="item" value="pizza" />
+        <input
+          type="checkbox"
+          name="item"
+          value="ピザ🍕"
+          onChange={handleChecked}
+          checked={checkedItem.includes("ピザ🍕")}
+        />
         ピザ🍕
       </label>
-      <div>{/* ここに選択されたアイテムを表示します */}</div>
+      <div>
+        {checkedItem.length === 0
+          ? "「何も選択されていません」"
+          : checkedItem.map((item) => item).join(", ")}
+      </div>
     </div>
   );
 };
