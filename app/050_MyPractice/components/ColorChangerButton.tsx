81c568b6c7ff088831455de7e3629a3778da27c0
kento <na.ken0627@gmail.com>
2025-06-09 22:29:22 +0900

    fix:ColorHangerButton

--- app/050_MyPractice/components/ColorChangerButton.tsx
+++ app/050_MyPractice/components/ColorChangerButton.tsx
@@ -1,16 +1,17 @@
-"use" client
+"use client";
 
 import React, { useState } from "react";
 
-const colorList = ["red", "blue", "green", "yellow"];
+const colorList = ["gray", "red", "blue", "green", "yellow"];
 
 function ColorChangerButton() {
   // ここにstateを作成して初期値を設定してください
-  const [color, setColor] = useState("grey");
+  const [color, setColor] = useState("gray");
 
   // ここにクリックで色を切り替える関数を作成してください
 
   const handleClick = () => {
+    console.log(color);
     const checkIndex = colorList.indexOf(color);
     let nextIndex = checkIndex + 1;
     if (nextIndex >= colorList.length) {
@@ -21,7 +22,10 @@ function ColorChangerButton() {
   };
 
   return (
-    <button onClick={handleClick} className="bg-amber-100">
+    <button
+      onClick={handleClick}
+      className={`bg-${color}-300 hover:bg-${color}-200 text-black p-2 rounded-md m-3 border border-amber-50`}
+    >
       色を変える
     </button>
   );
