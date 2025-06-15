a0aeea8f86781ac5f3bb244b1a444444b090c621
kento <na.ken0627@gmail.com>
2025-06-15 22:39:54 +0900

    feat:new Algorithm pattarn

--- app/050_MyPractice/components/Algorithm.tsx
+++ app/050_MyPractice/components/Algorithm.tsx
@@ -1,3 +1,7 @@
 export const Algorithm = () => {
+  const handleSplit = (texts: string, separator: string) => {
+    return texts.split(separator);
+  };
+  console.log(handleSplit("Apple,Banana,Cherry", ","));
   return <div></div>;
 };
