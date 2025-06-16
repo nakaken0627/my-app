14ffc63a953aa07e33ce7f6f1b6be832f84bc629
kento <na.ken0627@gmail.com>
2025-06-17 07:43:36 +0900

    feat:removeInvalidUsers

--- app/050_MyPractice/components/Algorithm.tsx
+++ app/050_MyPractice/components/Algorithm.tsx
@@ -1,7 +1,32 @@
 export const Algorithm = () => {
-  const handleSplit = (texts: string, separator: string) => {
-    return texts.split(separator);
+  // const handleSplit = (texts: string, separator: string) => {
+  //   return texts.split(separator);
+  // };
+  // console.log(handleSplit("Apple,Banana,Cherry", ","));
+
+  // 提供されたUSER配列を定義
+  const users = [
+    { name: "John", age: 25 },
+    { name: "Doe", age: null },
+    { name: "", age: 30 },
+    { name: "Anna", age: 22 },
+  ];
+
+  type User = {
+    name: string;
+    age: number | null;
   };
-  console.log(handleSplit("Apple,Banana,Cherry", ","));
+
+  // 条件に合わないユーザーを除いて新しい配列を作る関数を定義
+  function removeInvalidUsers(users: User[]) {
+    //ここに処理を記述してください。
+    return users.filter((user) => {
+      return user.name !== "" && user.age !== null;
+    });
+  }
+
+  // 関数を実行しconsole.logで結果を確認
+  console.log(removeInvalidUsers(users));
+
   return <div></div>;
 };
