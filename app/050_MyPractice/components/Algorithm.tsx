94f60f6b33c3fe65f0de2999881cb14492ca4778
kento <na.ken0627@gmail.com>
2025-06-19 09:20:27 +0900

    feat:e-mail address checker

--- app/050_MyPractice/components/Algorithm.tsx
+++ app/050_MyPractice/components/Algorithm.tsx
@@ -5,28 +5,37 @@ export const Algorithm = () => {
   // console.log(handleSplit("Apple,Banana,Cherry", ","));
 
   // 提供されたUSER配列を定義
-  const users = [
-    { name: "John", age: 25 },
-    { name: "Doe", age: null },
-    { name: "", age: 30 },
-    { name: "Anna", age: 22 },
-  ];
+  // const users = [
+  //   { name: "John", age: 25 },
+  //   { name: "Doe", age: null },
+  //   { name: "", age: 30 },
+  //   { name: "Anna", age: 22 },
+  // ];
 
-  type User = {
-    name: string;
-    age: number | null;
-  };
+  // type User = {
+  //   name: string;
+  //   age: number | null;
+  // };
 
   // 条件に合わないユーザーを除いて新しい配列を作る関数を定義
-  function removeInvalidUsers(users: User[]) {
-    //ここに処理を記述してください。
-    return users.filter((user) => {
-      return user.name !== "" && user.age !== null;
-    });
-  }
+  //   function removeInvalidUsers(users: User[]) {
+  //     //ここに処理を記述してください。
+  //     return users.filter((user) => {
+  //       return user.name !== "" && user.age !== null;
+  //     });
+  //   }
 
-  // 関数を実行しconsole.logで結果を確認
-  console.log(removeInvalidUsers(users));
+  //   // 関数を実行しconsole.logで結果を確認
+  //   console.log(removeInvalidUsers(users));
 
   return <div></div>;
+};
+
+const myRe = new RegExp(
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+);
+
+export const addressChecker = (address: string) => {
+  const result = myRe.test(address);
+  console.log(result);
 };
